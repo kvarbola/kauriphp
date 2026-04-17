2423428167530734e9a855055d1a65830d0cb7a3
kvarbola <kvarbola@hkhk.edu.ee>
2026-04-17 13:29:14 +0300

    PHP

---
+++ harj1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP harjutused</title>
+</head>
+<body>
+    <h1>Harjutus 01</h1>
+    <?php
+    //Kauri Varbola 17.04.2026
+    //Harjutus 01
+    $nimi = "Kauri";
+    $aasta = 2009;
+    $kuju = "vähk";
+    
+    echo "Minu nimi on $nimi.<br> Olen sündinud aastal $aasta.<br> Minu tähtkuju on $kuju.<br>";
+    echo "\"It's my life\" - Dr.Alban<br>";
+    echo "(\(\<br>( -.-)<br>o_(\")(\")<br>";
+    
+    ?>
+</body>
+</html>
---
+++ harj2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP harjutused</title>
+</head>
+<body>
+    <h1>Harjutus 01</h1>
+    <?php
+    //Kauri Varbola 17.04.2026
+    //Harjutus 02
+    
+    $x = 69;
+    $y = 57;
+    $liit = $x + $y;
+    $lahut = $x - $y;
+    $korr = $x * $y;
+    $jaga = $x / $y;
+    $jaak = $x % $y;
+
+    echo "$x + $y = $liit<br>$x - $y = $lahut<br>$x * $y = $korr<br>$x / $y = $jaga<br>$x % $y = $jaak";
+
+    ?>
+</body>
+</html>
